0a6ce196c89e527e999f7b2cd83f47917aadb3e5
yuricrotti <yuricrotti@hotmail.com>
2024-07-17 17:26:44 -0300

    Update  version on setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bees_ml_path_challenge-skeleton_package',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'pytest',
